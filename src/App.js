284a878740e51337987c8b7bf11e40405aaf7238
hashamuddin1 <73032957+hashamuddin1@users.noreply.github.com>
2024-01-01 22:28:19 +0500

    learn simple use effect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, React } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [multiplyTwo, setMultiplyTwo] = useState(1);
+
+  function insertCount() {
+    setCount(count + 1);
+  }
+
+  function multiplyTwoFunc() {
+    setMultiplyTwo(multiplyTwo * 2);
+  }
+
+  useEffect(() => {
+    console.log("Component Mounted");
+  },[multiplyTwo]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* use effect hook */}
+      {/* ye by default may page load may run hota hay */}
+      {/* agar ham chahtay hay k har bar ye run na hotw tw isko bhi handle krsaktay hay */}
+      {/* uses */}
+      {/* 1- data page load may fetch krne k liye
+      2- ya tw DOM ko directly update krna hota hay */}
+
+      <h1>Button Clicked {count} Times</h1>
+      <button onClick={insertCount}>Click Me To Add</button>
+      <h1>Multiply By Two Value {multiplyTwo}</h1>
+      <button onClick={multiplyTwoFunc}>Click Me To Multiply</button>
+    </>
   );
 }
 
